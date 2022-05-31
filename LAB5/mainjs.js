75786ba0375c6fca34670a85dcb3f9a992bfa16f
2013147511 <ohjasung@naver.com>
2022-06-01 06:58:00 +0900

    LAB5.18

---
+++ LAB5/mainjs.js
@@ -0,0 +1,20 @@
+function loadimage(){
+    return fetch("product.json")
+     .then((response)=>response.json())
+     .then((json)=>json.pizza)
+
+}
+
+loadimage().then((pizza)=>{
+         console.log(pizza);
+         showpizza(pizza);
+});
+
+function showpizza(pizza){
+    const container = document.querySelector(".pizza")
+    container.innerHTML = pizzas.map((pizza)=>createHTMLIMAGE(pizza)).join("");
+}
+
+function createHTMLIMAGE(pizza){
+    return '<div class="imgbox"><img src="${pizza.image}" class="myimg" id="myimg1"></div> ';
+}
