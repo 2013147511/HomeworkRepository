40c3b366343b05f966388c4e1a7ca17cd2758317
2013147511 <ohjasung@naver.com>
2022-06-09 03:40:21 +0900

    1.0

---
+++ api-project/signoid.js
@@ -0,0 +1,4 @@
+function printName()  {
+    const myFullCreditsForGrad = document.getElementById('myFullCreditsForGrad').value;
+    document.getElementById("result").innerText = myFullCreditsForGrad;
+  }
