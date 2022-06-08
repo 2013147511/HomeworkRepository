2f71397649b88e4e473602d66bff98f662be099c
2013147511 <ohjasung@naver.com>
2022-06-09 04:40:32 +0900

    1.0

--- api-project/signoid.js
+++ api-project/signoid.js
@@ -1,4 +1,4 @@
-var percent;
+var percent=0;
 
 function printmyFullCreditsForGrad(){
     const myFullCreditsForGrad = document.getElementById('myFullCreditsForGrad').value;
@@ -9,11 +9,25 @@ function printmyFullCreditsForGrad(){
 function printmySemesters(){
     const mySemesters = document.getElementById('mySemesters').value;
     document.getElementById("result2").innerText = mySemesters;
+    if(mySemesters<3){
+        percent+= -0.33;
+    }else if(mySemesters<5){
+        percent+= 0;
+    }else if(mySemesters<7){
+        percent+= -0.36;
+    }else if(mySemesters<9){
+        percent+= -1.19;
+    }else{
+        percent+= 12.65;
+    }
 };
 
 function printmyAppliedForGrad(){
     const myAppliedForGrad = document.getElementById('myAppliedForGrad').value;
     document.getElementById("result3").innerText = myAppliedForGrad;
+    if(myAppliedForGrad==1){
+        percent+=-0.22;
+    }
 };
 
 function printselectCourse(){
@@ -24,9 +38,11 @@ function printselectCourse(){
 function printmyCredits(){
     const myCredits = document.getElementById('myCredits').value;
     document.getElementById("result5").innerText = myCredits;
+    percent+=3.56*myCredits;
 };
 
 function printmyLastSemesterCredits(){
     const myLastSemesterCredits = document.getElementById('myLastSemesterCredits').value;
-    document.getElementById("result6").innerText = myLastSemesterCredits;
+    percent+=0.25*myLastSemesterCredits;
+    document.getElementById("result6").innerText = percent;
 };
