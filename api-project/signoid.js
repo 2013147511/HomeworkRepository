1fcfa8f9dbafc5ea894cdc51ab0defd15d332d72
2013147511 <ohjasung@naver.com>
2022-06-09 04:55:54 +0900

    1.0

--- api-project/signoid.js
+++ api-project/signoid.js
@@ -5,7 +5,7 @@ function printmyFullCreditsForGrad(){
     document.getElementById("result1").innerText = myFullCreditsForGrad;
   };
 
-
+//학년에 따라 수강신청 확률 변경
 function printmySemesters(){
     const mySemesters = document.getElementById('mySemesters').value;
     document.getElementById("result2").innerText = mySemesters;
@@ -21,7 +21,7 @@ function printmySemesters(){
         percent+= 12.65;
     }
 };
-
+//졸업신청여부에 따라 수강신청 확률 변경
 function printmyAppliedForGrad(){
     const myAppliedForGrad = document.getElementById('myAppliedForGrad').value;
     document.getElementById("result3").innerText = myAppliedForGrad;
@@ -29,20 +29,33 @@ function printmyAppliedForGrad(){
         percent+=-0.22;
     }
 };
-
+//선택한과목에 따라 수강신청 확률 변경
 function printselectCourse(){
     const selectCourse = document.getElementById('selectCourse').value;
     document.getElementById("result4").innerText = selectCourse;
+    switch(selectCourse){
+        case 1: percent+=90*0.14-69*0.05+2.58*0.28-13.85+75*0.05;     // 정원 / 참여인원 / 평점 / 실시간온라인여부 / 전공자정원수
+        break;
+        case 2: percent+=80*0.14-145*0.05+3.3*0.28-13.85+56*0.05;
+        break;
+        case 3: percent+=100*0.14-177*0.05+3.81*0.28-13.85+70*0.05;
+        break;
+        case 4: percent+=140*0.14-161*0.05+4.17*0.28-13.85+120*0.05;
+        break;
+        case 5: percent+=90*0.14 -80*0.05+4*0.28-13.85+63*0.05;   //김영석교수님 분반으로 설정
+        break;
+    }
 };
-
+//총이수학점에 따라 수강신청 확률 변경
 function printmyCredits(){
     const myCredits = document.getElementById('myCredits').value;
     document.getElementById("result5").innerText = myCredits;
     percent+=3.56*myCredits;
 };
-
+//직전학기이수학점에 따라 수강신청 확률 변경
 function printmyLastSemesterCredits(){
     const myLastSemesterCredits = document.getElementById('myLastSemesterCredits').value;
     percent+=0.25*myLastSemesterCredits;
-    document.getElementById("result6").innerText ="이 과목의 수강신청성공확률은" +percent+"입니다";
+    const signoid=1/(1+Math.exp(percent));
+    document.getElementById("result6").innerText ="이 과목의 수강신청성공확률은" +signoid+"%입니다";
 };
