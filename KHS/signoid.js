7f59a62ae2e20c553b4543f2824d18c4e67a9cd8
2013147511 <ohjasung@naver.com>
2022-06-10 04:45:18 +0900

    1.0

--- KHS/signoid.js
+++ KHS/signoid.js
@@ -149,7 +149,7 @@ function signoidReady(){
     signoidReady1 = signoidReady1 + (lastCredits/averageCredits)*0.25;   //직전학기이수학점_학기당이수학점 * 알맞은상수를 더해줍니다
 
 
-    document.getElementById("readyre").innerText = signoidReady1+14;
+    document.getElementById("readyre").innerText = totalCredits+14;
 
 }
 
