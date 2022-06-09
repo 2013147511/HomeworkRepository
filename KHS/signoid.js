4dede630e8f5f50b5d1f3150b7d85f523b74f620
2013147511 <ohjasung@naver.com>
2022-06-10 02:04:04 +0900

    1.0

--- KHS/signoid.js
+++ KHS/signoid.js
@@ -3,14 +3,49 @@ var finalPercent=1;
 var signoid=0; 
 var selectCourseName;
 var multicsMileage=1;
+var courseScedule = new object();
+
+
+function signoidReady(){
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function initPercent(){
     percent=0;
     signoid=0;
 }
-var courseScedule = new object();
+
+
 function printmyFullCreditsForGrad(){
     const myFullCreditsForGrad = document.getElementById('myFullCreditsForGrad').value;
   };
+
 
 //학년에 따라 수강신청 확률 변경
 function printmySemesters(){
@@ -234,17 +269,3 @@ function addCoursetoTable(){
 
 };
 
-function onSignIn(googleUser) {
-    // Useful data for your client-side scripts:
-    var profile = googleUser.getBasicProfile();
-    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-    console.log('Full Name: ' + profile.getName());
-    console.log('Given Name: ' + profile.getGivenName());
-    console.log('Family Name: ' + profile.getFamilyName());
-    console.log("Image URL: " + profile.getImageUrl());
-    console.log("Email: " + profile.getEmail());
-
-    // The ID token you need to pass to your backend:
-    var id_token = googleUser.getAuthResponse().id_token;
-    console.log("ID Token: " + id_token);
-  };
