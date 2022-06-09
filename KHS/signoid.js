b36b9f0adee64a351952b7d3ef7200cbc6cfc0a1
2013147511 <ohjasung@naver.com>
2022-06-10 02:32:23 +0900

    1.0

--- KHS/signoid.js
+++ KHS/signoid.js
@@ -9,7 +9,7 @@ var courseScedule = new object();
 function signoidReady(){
     var myMajor = document.getElementsByName("myMajor");
     alert('선택된 옵션 text 값=' + myMajor.options[myMajor.selectedIndex].text); 
-    document.getElementById("readyre").innerText = "12"+mymajor.options[mymajor.selectedIndex].value +"11";
+    document.getElementById("readyre").innerText = "11";
 }
 
 
