0170de93104641d789e7aecfb58ebdc3abb95402
2013147511 <ohjasung@naver.com>
2022-06-10 08:37:12 +0900

    1.0

--- KHS/signoid.js
+++ KHS/signoid.js
@@ -361,6 +361,7 @@ function addCoursetoTable(){
         default : 
         break;
     }
+    console.log(probability);
     document.getElementById("#calc-result-number").innerText =  probability.toFixed(2);  // 총확률표시 부분에 확률 표시.
 };
 
